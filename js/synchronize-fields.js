b417993d62b07b14e7137e705c71eaf90e96781a
Elianta <allie.que.sonrie@gmail.com>
2017-09-05 15:13:29 +0300

    Refactoring the names of attributes.

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,11 +1,11 @@
 'use strict';
 (function () {
-  window.synchronizeFields = function (element1, element2, element1fields, element2fields, syncValues) {
-    element1.addEventListener('change', function () {
-      var selectedOption = element1.selectedOptions[0].value;
-      for (var i = 0; i < element1fields.length; i++) {
-        if (element1fields[i] === selectedOption) {
-          syncValues(element2, element2fields[i]);
+  window.synchronizeFields = function (eventTargetElement, elementToBeSynchronized, eventTargetElementOptionsArray, elementToBeSynchronizedOptionsArray, syncValues) {
+    eventTargetElement.addEventListener('change', function () {
+      var selectedOption = eventTargetElement.selectedOptions[0].value;
+      for (var i = 0; i < eventTargetElementOptionsArray.length; i++) {
+        if (eventTargetElementOptionsArray[i] === selectedOption) {
+          syncValues(elementToBeSynchronized, elementToBeSynchronizedOptionsArray[i]);
         }
       }
     });
